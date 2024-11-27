1192bff3981695fb36617010c44bb1b19bb98792
Sammy Alawar <sna61@mail.aub.edu>
2024-11-27 18:44:48 +0300

    Deleted garbage file

--- Customers/schemas.py
+++
@@ -1,6 +0,0 @@
-from flask_marshmallow import Marshmallow
-
-ma = Marshmallow()
-class CustomerSchema(ma.Schema):
-    class Meta:
-        fields = ("id", "full_name", "username", "age", "address", "gender", "marital_status", "wallet")
